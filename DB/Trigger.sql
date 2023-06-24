1b02c93d6b78d94ffd9f6ab03cba1ac3dbdcb752
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-06-24 11:48:09 +0200

    trigger (da testare)

--- DB/Trigger.sql
+++ DB/Trigger.sql
@@ -1,8 +1,5 @@
 
 
--- ************************* INSERT TRIGGER ************************* --
--- *************************      START     ************************* --
-
 -- check validità data_inizio di un Evento (INSERT)
 DELIMITER $$
 DROP TRIGGER IF EXISTS valida_data_inizio_evento_insert;
@@ -16,6 +13,22 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- check validità data_inizio di un Evento (UPDATE)
+DROP TRIGGER IF EXISTS valida_data_inizio_evento_update;
+DELIMITER $$
+CREATE TRIGGER valida_data_inizio_evento_update
+BEFORE UPDATE ON Evento
+FOR EACH ROW
+BEGIN
+    IF (NEW.data_inizio < CURDATE())
+    THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La data di inizio non può essere precedente a oggi.';
+    END IF;
+END$$
+DELIMITER ;
+
+
+-- ******************************************************************** --
 
 
 -- check validità data_fine di un Evento (INSERT)
@@ -33,6 +46,25 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- check validità data_fine di un Evento (UPDATE)
+DROP TRIGGER IF EXISTS valida_data_fine_evento_update;
+DELIMITER $$
+CREATE TRIGGER valida_data_fine_evento_update
+BEFORE UPDATE ON Evento
+FOR EACH ROW
+BEGIN
+    IF (DATE(NEW.data_fine) < DATE(NEW.data_inizio))
+    THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La data di fine non può essere precedente alla data di inizio.';
+    ELSEIF (DATE(NEW.data_fine) > DATE(NEW.data_inizio) AND DATE(NEW.data_fine) <> DATE(NEW.data_inizio))
+    THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La data di fine deve essere nella stessa giornata della data di inizio.';
+    END IF;
+END$$
+DELIMITER ;
+
+
+-- ******************************************************************** --
 
 
 -- check orario con intervallo di 15 minuti (INSERT)
@@ -48,6 +80,21 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- check orario con intervallo di 15 minuti (UPDATE)
+DROP TRIGGER IF EXISTS valida_orario_evento_update;
+DELIMITER $$
+CREATE TRIGGER valida_orario_evento_update
+BEFORE UPDATE ON Evento
+FOR EACH ROW
+BEGIN
+    IF MINUTE(NEW.data_inizio) % 15 != 0 OR MINUTE(NEW.data_fine) % 15 != 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'L`orario di inizio e di fine evento devono essere multipli di 15 minuti.';
+    END IF;
+END$$
+DELIMITER ;
+
+
+-- ******************************************************************** --
 
 
 -- check disponibilità aula per creazione di un evento (INSERT)
@@ -94,106 +141,6 @@ BEGIN
 END $$
 DELIMITER ;
 
--- ************************* INSERT TRIGGER ************************* --
--- *************************       END      ************************* --
-
-
--- ------------------------------------------------------------------ --
-
-
--- ************************* CALL PROCEDURE ************************* --
--- *************************      START     ************************* --
-
--- check generazione eventi ricorrenti se necessario (funzione) - (INSERT)
-DROP TRIGGER IF EXISTS genera_eventi_trigger_insert;
-DELIMITER $$
-CREATE TRIGGER genera_eventi_trigger_insert
-AFTER INSERT ON Evento
-FOR EACH ROW
-BEGIN
-    IF (NEW.tipo_ricorrenza IS NOT NULL)
-    THEN
-        CALL genera_eventi(NEW.id, NEW.id_aula, NEW.data_inizio, NEW.data_fine, NEW.tipo_ricorrenza, NEW.data_fine_ricorrenza);
-    END IF;
-END$$
-DELIMITER ;
-
-
--- check generazione eventi ricorrenti se necessario (funzione) - (UPDATE)
-DROP TRIGGER IF EXISTS genera_eventi_trigger_update;
-DELIMITER $$
-CREATE TRIGGER genera_eventi_trigger_update
-AFTER UPDATE ON Evento
-FOR EACH ROW
-BEGIN
-    IF NEW.tipo_ricorrenza IS NOT NULL THEN
-        -- Chiamata alla procedura per generare gli eventi ricorrenti
-        CALL genera_eventi(NEW.ID, NEW.ID_aula, NEW.data_inizio, NEW.data_fine, NEW.tipo_ricorrenza, NEW.data_fine_ricorrenza);
-    END IF;
-END$$
-DELIMITER ;
-
-
--- ************************* CALL PROCEDURE ************************* --
--- *************************       END      ************************* --
-
-
--- ------------------------------------------------------------------ --
-
-
--- ************************* UPDATE TRIGGER ************************* --
--- *************************      START     ************************* --
-
--- check validità data_inizio di un Evento (UPDATE)
-DROP TRIGGER IF EXISTS valida_data_inizio_evento_update;
-DELIMITER $$
-CREATE TRIGGER valida_data_inizio_evento_update
-BEFORE UPDATE ON Evento
-FOR EACH ROW
-BEGIN
-    IF (NEW.data_inizio < CURDATE())
-    THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La data di inizio non può essere precedente a oggi.';
-    END IF;
-END$$
-DELIMITER ;
-
-
-
--- check validità data_fine di un Evento (UPDATE)
-DROP TRIGGER IF EXISTS valida_data_fine_evento_update;
-DELIMITER $$
-CREATE TRIGGER valida_data_fine_evento_update
-BEFORE UPDATE ON Evento
-FOR EACH ROW
-BEGIN
-    IF (DATE(NEW.data_fine) < DATE(NEW.data_inizio))
-    THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La data di fine non può essere precedente alla data di inizio.';
-    ELSEIF (DATE(NEW.data_fine) > DATE(NEW.data_inizio) AND DATE(NEW.data_fine) <> DATE(NEW.data_inizio))
-    THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La data di fine deve essere nella stessa giornata della data di inizio.';
-    END IF;
-END$$
-DELIMITER ;
-
-
-
--- check orario con intervallo di 15 minuti (UPDATE)
-DROP TRIGGER IF EXISTS valida_orario_evento_update;
-DELIMITER $$
-CREATE TRIGGER valida_orario_evento_update
-BEFORE UPDATE ON Evento
-FOR EACH ROW
-BEGIN
-    IF MINUTE(NEW.data_inizio) % 15 != 0 OR MINUTE(NEW.data_fine) % 15 != 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'L`orario di inizio e di fine evento devono essere multipli di 15 minuti.';
-    END IF;
-END$$
-DELIMITER ;
-
-
-
 -- check disponibilità aula per creazione di un evento (UPDATE)
 DELIMITER $$
 DROP TRIGGER IF EXISTS verifica_disponibilita_aula_update;
@@ -239,5 +186,41 @@ END $$
 DELIMITER ;
 
 
--- ************************* UPDATE TRIGGER ************************* --
--- *************************       END      ************************* --
+-- ******************************************************************** --
+
+
+-- check generazione eventi ricorrenti se necessario (procedura) - (INSERT)
+DROP TRIGGER IF EXISTS genera_eventi_trigger_insert;
+DELIMITER $$
+CREATE TRIGGER genera_eventi_trigger_insert
+AFTER INSERT ON Evento
+FOR EACH ROW
+BEGIN
+    IF (NEW.tipo_ricorrenza IS NOT NULL)
+    THEN
+        CALL genera_eventi_ricorrenti_procedura(NEW.id, NEW.id_aula, NEW.data_inizio, NEW.data_fine, NEW.tipo_ricorrenza, NEW.data_fine_ricorrenza);
+    END IF;
+END$$
+DELIMITER ;
+
+
+-- check generazione eventi ricorrenti se necessario (procedura) - (UPDATE)
+DROP TRIGGER IF EXISTS genera_eventi_trigger_update;
+DELIMITER $$
+CREATE TRIGGER genera_eventi_trigger_update
+AFTER UPDATE ON Evento
+FOR EACH ROW
+BEGIN
+    IF NEW.tipo_ricorrenza IS NOT NULL THEN
+        CALL genera_eventi_ricorrenti_procedura(NEW.id, NEW.id_aula, NEW.data_inizio, NEW.data_fine, NEW.tipo_ricorrenza, NEW.data_fine_ricorrenza);
+    END IF;
+END$$
+DELIMITER ;
+
+
+-- ******************************************************************** --
+
+
+
+
+
