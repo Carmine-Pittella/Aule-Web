e591d6350b492c267578f75a9b77bc4fd2ee346f
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-06-25 19:57:06 +0200

    aggiunta delle classi in Model

---
+++ src/main/java/it/univaq/f4i/iw/Aule_Web/data/model/Aula.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.univaq.f4i.iw.Aule_Web.data.model;
+
+/**
+ *
+ * @author Carmine
+ */
+public class Aula {
+    
+}
