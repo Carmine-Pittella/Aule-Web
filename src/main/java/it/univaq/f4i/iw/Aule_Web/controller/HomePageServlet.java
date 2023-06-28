00edc6a285b14125e6bc91179ea4bc77343babe3
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-06-28 12:35:39 +0200

    Pulizia file di test + aggiunta HomePageServlet

---
+++ src/main/java/it/univaq/f4i/iw/Aule_Web/controller/HomePageServlet.java
@@ -0,0 +1,22 @@
+
+package it.univaq.f4i.iw.Aule_Web.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Carmine
+ */
+
+public class HomePageServlet extends AuleWebBaseController {
+
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'processRequest'");
+    }
+
+}
